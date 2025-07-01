0927f9d9a83091b1c5f89d7446e1d44a6826d534
bakhytzhan <bakhytzhanabdilmazhit@gmail.com>
2025-07-01 07:22:39 +0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.viewpager2)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
